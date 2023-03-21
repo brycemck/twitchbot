cc03bc9090b66ffc9c82398195bd5a407877fdcf
Bryce McKenney <brycemckenney@gmail.com>
2023-03-21 01:03:02 -0700

    removed unnecessary vite config

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        
-      }
-    }
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./client', import.meta.url))
